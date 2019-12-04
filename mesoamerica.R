0dd8ee05ddeb145b6bc30a83be67f1e1b05d9336
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2019-12-03 21:31:48 -0500

    experiment with fasterize

--- mesoamerica.R
+++ mesoamerica.R
@@ -1,4 +1,8 @@
 library(stringr)
+library(sf)
+library(raster)
+library(rgdal)
+library(fasterize)
 ####----
 Species <- read.delim("acuatica.txt", encoding = 'UTF-8')
 Species$habit <- 'H.A'
@@ -98,3 +102,76 @@ merge(NA_Flora[,c('Scientific.Name','HabitSymbol')],
 
 NA_Floramerge$HabitSymbol <- as.character(NA_Floramerge$HabitSymbol)
 filtermerged <- subset(NA_Floramerge, !is.na(NA_Floramerge$habit))
+
+#----
+
+#Model Species Distribution
+#Positive point data weighted 100%.
+#Negative data weighted inverse of collective point data density.
+#Positive state/county data inversely wieghted based on surface area.
+#Distance from positive data can be used as training data, if model can weigh it.
+#Model would probably use it as the primary variable, because by definition all inputs will be at zero distance from the points and therefore only will predict exactly the known locations.It may be more appropriate to multiply a proximity factor to the final prediction before selecting species rate of presence threshold.
+#Distance from positive data can be used to weigh negative data. Greater distance lower weight. Lower density of total point data, lower weight.
+
+
+Species <- read.delim("acuatica.txt", encoding = 'UTF-8')
+USASpp <- read.delim('C:/a/bio/biodiversity/2019/0007369-191105090559680/0007369-191105090559680.csv')
+testSpp <- read.delim('C:/a/bio/biodiversity/2019/0007389-191105090559680/0007389-191105090559680.csv')
+testSpp <- read.delim('C:/a/bio/biodiversity/2019/0007993-191105090559680/0007993-191105090559680.csv')
+testSpp$decimalLatitude <- as.numeric(as.character(testSpp$decimalLatitude))
+testSpp$decimalLongitude <- as.numeric(as.character(testSpp$decimalLongitude))
+
+testSppinat <- subset(testSpp, institutionCode %in% 'iNaturalist' & species %in% 'Valeriana sitchensis')
+testSppnotinat <- subset(testSpp, !institutionCode %in% 'iNaturalist' & species %in% 'Valeriana sitchensis')
+
+
+plot(testSppinat$decimalLongitude, testSppinat$decimalLatitude, 
+     col = rgb(red = 0, green = 1, blue = 0, alpha = 0.15))
+points(testSppnotinat$decimalLongitude, testSppnotinat$decimalLatitude,
+       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.15), new=FALSE)
+testagg <- aggregate(testSpp[,'species'], by=list(testSpp$species), FUN='length')
+testSpp2 <- unique(testSpp[,c('species','decimalLongitude', 'decimalLatitude')])
+
+#----
+#process spatial data points to raster?
+
+path = 'C:/workspace2/treeatlas/nam5k/'
+Tw <- raster(paste0(path, 'Tw.tif'))
+res(Tw)
+crs(Tw)
+# make simplefeatures spatial dataframe ----------------------------------------------------------
+
+fips_sf <- st_read('C:/a/geo/base/americas4.shp')
+fips_sfna <- subset(fips_sf, !GROUP %in% c('South America', 'Bermuda','Hawaii' ))
+fips_sfna$ID <- as.numeric(fips_sfna$ID)
+
+fip_sfna1 <- st_transform(fips_sfna, crs(Tw))
+narast <- fasterize(fip_sfna1, Tw, field = 'ID', fun = "last")
+plot(narast)
+points1 <- as.data.frame(rasterToPoints(Tw))
+points2 <- as.data.frame(rasterToPoints(narast))
+points2 <- merge(points2, st_drop_geometry(fips_sfna[,c('ID','FIPS')]), by.x='layer',by.y='ID', all.x = TRUE)
+points3 <- merge(points1, points2, by=c('x','y'))
+#preproject
+fips_sfnarepro <- st_transform(fips_sfna, st_crs(Tw))
+plot(fips_sfnarepro[1])
+namap <- st_rasterize(fips_sfnarepro, Tw, field="GROUP", fun='last', background=NA)
+ptsfit<-st_rasterize(ptsALL,Tw,field="best", fun='last', background=NA)
+
+
+
+
+
+library(raster)
+library(fasterize)
+library(sf)
+p1 <- rbind(c(-180,-20), c(-140,56), c(10, 0), c(-140,-60), c(-180,-20))
+hole <- rbind(c(-150,-20), c(-100,-10), c(-110,20), c(-150,-20))
+p1 <- list(p1, hole)
+p2 <- list(rbind(c(-10,0), c(140,60), c(160,0), c(140,-55), c(-10,0)))
+p3 <- list(rbind(c(-125,0), c(0,60), c(40,5), c(15,-45), c(-125,0)))
+pols <- st_sf(value = c(1,2,3),
+              geometry = st_sfc(lapply(list(p1, p2, p3), st_polygon)))
+r <- raster(pols, res = 1)
+r <- fasterize(pols, r, field = "value", fun="sum")
+plot(r)
